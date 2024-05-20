ab0e424e76e66d628da07735eb96ce1ea9fcbeb0
김보경 <rlaqhrud6263@naver.com>
2024-05-21 00:50:38 +0900

    Feat: svgr / webpack 설치

---
+++ src/app/providers.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import * as React from 'react';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (typeof window === 'undefined') {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+export function Providers(props: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration>
+        {props.children}
+      </ReactQueryStreamedHydration>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
--- src/hooks/useReactQuery.tsx
+++ src/hooks/useReactQuery.tsx
@@ -1,11 +1,9 @@
 'use client';
-import { useState } from 'react';
+import { PropsWithChildren, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-export default function ReactQueryProviders({
-  children,
-}: React.PropsWithChildren) {
+export default function ReactQueryProviders({ children }: PropsWithChildren) {
   const [queryClient] = useState(
     () =>
       new QueryClient({
