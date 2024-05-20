9009a800642c7e2d79c138cf5099b9f65a3b77dc
김보경 <rlaqhrud6263@naver.com>
2024-05-20 23:45:07 +0900

    Fix: eslint 재설정

--- src/hooks/useReactQuery.tsx
+++ src/hooks/useReactQuery.tsx
@@ -1,9 +1,11 @@
-'use client'
-import { useState } from 'react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+'use client';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-export default function ReactQueryProviders({ children }: React.PropsWithChildren) {
+export default function ReactQueryProviders({
+  children,
+}: React.PropsWithChildren) {
   const [queryClient] = useState(
     () =>
       new QueryClient({
@@ -13,12 +15,12 @@ export default function ReactQueryProviders({ children }: React.PropsWithChildre
           },
         },
       }),
-  )
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  )
+  );
 }
