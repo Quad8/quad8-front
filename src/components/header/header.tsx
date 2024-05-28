8d2da1e670928083d40120c69fead8e8ea7de2c6
armd482 <armd482@naver.com>
2024-05-28 17:23:31 +0900

    Design: header 컴포넌트 버튼 아이콘 변경

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,15 @@
 import classNames from 'classnames/bind';
+import Logo from '@/public/svgs/logo.svg';
+import User from '@/public/svgs/user.svg';
+import Cart from '@/public/svgs/cart.svg';
+import Link from 'next/link';
 import styles from './header.module.scss';
+
+const PAGE_BUTTON = [
+  { name: '커스텀 키보드 만들기', href: '/' },
+  { name: 'SHOP', href: '/' },
+  { name: '커뮤니티', href: '/' },
+];
 
 export default function Header() {
   const cn = classNames.bind(styles);
@@ -7,19 +17,27 @@ export default function Header() {
   return (
     <div className={cn('wrapper')}>
       <div className={cn('right-wrapper')}>
-        <div>로고</div>
+        <Link href="/" className={cn('logo')}>
+          <Logo width={71.44} height={32} />
+        </Link>
         <div className={cn('button-wrapper')}>
-          <div>커스텀 키보드 만들기</div>
-          <div>shop</div>
-          <div>커뮤니티</div>
+          {PAGE_BUTTON.map((element) => (
+            <Link key={element.name} href={element.href}>
+              {element.name}
+            </Link>
+          ))}
         </div>
       </div>
       <div className={cn('left-wrapper')}>
         <div>검색창</div>
         <div className={cn('status-wrapper')}>
-          <div>로그인</div>
-          <div>마이페이지</div>
-          <div>장바구니</div>
+          <Link href="/signin">로그인</Link>
+          <Link href="/mypage" className={cn('icon')}>
+            <User width={24} height={24} />
+          </Link>
+          <Link href="/cart" className={cn('icon')}>
+            <Cart width={24} height={24} />
+          </Link>
         </div>
       </div>
     </div>
