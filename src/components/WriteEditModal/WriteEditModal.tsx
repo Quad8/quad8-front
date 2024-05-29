9b025bcaeeb5316b16691865157d8b340cde0eb0
minjeong9919 <minjeong9919@naver.com>
2024-05-29 14:47:02 +0900

    Feat: portal 상황별로 디자인 다른 부분 구현

--- src/components/WriteEditModal/WriteEditModal.tsx
+++ src/components/WriteEditModal/WriteEditModal.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import KEYBOARD_DATA from '@/app/mj/customData';
+import { WriteEditModalType } from '@/constants/writeEditModalType';
 import classNames from 'classnames/bind';
 import styles from './WriteEditModal.module.scss';
 import KeyboardInfoBox from './KeyboardInfoBox';
 import TwoButton from '../buttons/TwoButton/TwoButton';
 import ImageInput from './ImageInput';
 
-// interface WriteEditModalProps {
-// }
+interface WriteEditModalProps {
+  type: WriteEditModalType;
+}
 
-export default function WriteEditModal() {
+export default function WriteEditModal({ type }: WriteEditModalProps) {
   const cn = classNames.bind(styles);
   const handleClickLeftButton = () => {
     /** 닫기버튼 누르면 실행되는 함수 */
@@ -19,7 +21,7 @@ export default function WriteEditModal() {
   };
   return (
     <div className={cn('container')}>
-      <KeyboardInfoBox keyboardInfo={KEYBOARD_DATA} isReview />
+      <KeyboardInfoBox keyboardInfo={KEYBOARD_DATA} isReview={type !== WriteEditModalType.writePost} />
       <div className={cn('input-div')}>
         <input className={cn('input')} placeholder="제목 작성" />
         <ImageInput />
