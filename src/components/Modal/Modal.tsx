d1d6be1b25f6caba3dab641f5f03f730a5683fbe
armd482 <armd482@naver.com>
2024-05-29 16:46:03 +0900

    Refactor: modal props PropsWithChidren 활용한 변경

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,21 +1,17 @@
 'use client';
 
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, PropsWithChildren } from 'react';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames/bind';
 import { useOutsideClick } from '@/hooks/useOutsideClick';
 import styles from './Modal.module.scss';
 
-interface ModalPortalProps {
-  children: React.ReactNode;
-}
-
-interface ModalProps extends ModalPortalProps {
+interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
-function ModalPortal({ children }: ModalPortalProps) {
+function ModalPortal({ children }: PropsWithChildren) {
   const [portalElement, setPortalElement] = useState<Element | null>(null);
   useEffect(() => {
     setPortalElement(document.getElementById('modal'));
@@ -24,7 +20,7 @@ function ModalPortal({ children }: ModalPortalProps) {
   return ReactDOM.createPortal(children, portalElement) as JSX.Element;
 }
 
-export default function Modal({ children, isOpen, onClose }: ModalProps) {
+export default function Modal({ children, isOpen, onClose }: PropsWithChildren<ModalProps>) {
   const cn = classNames.bind(styles);
   const modalRef = useRef<HTMLDivElement>(null);
   useOutsideClick(modalRef, onClose);
