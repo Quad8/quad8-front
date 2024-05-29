4b74d8b3640773ea805aae7a82ce304a680acc41
armd482 <armd482@naver.com>
2024-05-30 02:10:39 +0900

    Style: SVG 컴포넌트명 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,8 @@
 import { cookies } from 'next/headers';
 import classNames from 'classnames/bind';
-import Logo from '@/public/svgs/logo.svg';
-import User from '@/public/svgs/user.svg';
-import Cart from '@/public/svgs/cart.svg';
+import Ic_Logo from '@/public/svgs/logo.svg';
+import Ic_User from '@/public/svgs/user.svg';
+import Ic_Cart from '@/public/svgs/cart.svg';
 import Link from 'next/link';
 import styles from './Header.module.scss';
 import SearchBox from './SearchBox';
@@ -19,7 +19,7 @@ export default function Header() {
     <div className={cn('wrapper')}>
       <div className={cn('right-wrapper')}>
         <Link href="/" className={cn('logo')}>
-          <Logo width={130.65} height={23.89} />
+          <Ic_Logo width={130.65} height={23.89} />
         </Link>
         <div className={cn('button-wrapper')}>
           <Link href="/" className={cn('button')}>
@@ -36,10 +36,10 @@ export default function Header() {
         <div className={cn('status-wrapper')}>
           {!accessToken ? <LoginButton /> : <LogoutButton />}
           <Link href="/mypage" className={cn('icon')}>
-            <User width={48.08} height={48.08} />
+            <Ic_User width={48.08} height={48.08} />
           </Link>
           <Link href="/cart" className={cn('icon')}>
-            <Cart width={24} height={24} />
+            <Ic_Cart width={24} height={24} />
           </Link>
         </div>
       </div>
