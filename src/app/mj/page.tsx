4a636590fd2b505587b61707e803aabd5f8e8f7f
minjeong9919 <minjeong9919@naver.com>
2024-05-29 21:07:42 +0900

    Feat: 헤더의 가시성에 따른 버튼 유무

--- src/app/mj/page.tsx
+++ src/app/mj/page.tsx
@@ -1,3 +1,21 @@
+'use client';
+
+import { useRef } from 'react';
+import ScrollUpButton from '@/components/buttons/ScrollUpButton';
+import classNames from 'classnames/bind';
+import styles from './page.module.scss';
+
 export default function Page() {
-  return <div>테스트페이지 mj</div>;
+  const cn = classNames.bind(styles);
+  const headerRef = useRef(null);
+  return (
+    <div className={cn('container')}>
+      <header className={cn('header')} ref={headerRef}>
+        헤더
+      </header>
+      <div className={cn('content')}>내용</div>
+      <div className={cn('footer')}>푸터</div>
+      <ScrollUpButton headerRef={headerRef} />
+    </div>
+  );
 }
