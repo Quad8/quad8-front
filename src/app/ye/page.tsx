b1bb353022df44c66ff81f7c0aad350d5641697f
Young2un <popduddms@gmail.com>
2024-05-31 03:12:01 +0900

    Test: 테스트파일작성

--- src/app/ye/page.tsx
+++ src/app/ye/page.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
-  return <div>테스트페이지 ye</div>;
+import Pagination from '@/components/Pagination/Pagination';
+
+interface PageProps {
+  searchParams: {
+    page: string;
+  };
+}
+export default function Page({ searchParams }: PageProps) {
+  return <Pagination page={searchParams.page} count={161} limit={16} />;
 }
