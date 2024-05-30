5f108602e6429b6f869435ec596e1cf8bad6a7d0
armd482 <armd482@naver.com>
2024-05-31 00:03:14 +0900

    Fix: step context status 제거

--- src/context/customKeyboardContext.tsx
+++ src/context/customKeyboardContext.tsx
@@ -3,16 +3,8 @@
 import { PropsWithChildren, createContext, useCallback, useMemo, useState } from 'react';
 import { KEY, TEN_KEY } from '@/constants/keyboardData';
 
-interface StepStatusType {
-  board: boolean;
-  switch: boolean;
-  keyCap: boolean;
-}
-
 interface StepContextType {
-  stepStatus: StepStatusType;
   currentStep: 'board' | 'switch' | 'keyCap';
-  updateStepStatus: (step: 'board' | 'switch' | 'keyCap', status: boolean) => void;
   updateCurrentStep: (data: 'board' | 'switch' | 'keyCap') => void;
 }
 
@@ -47,13 +39,7 @@ interface KeyColorContextType {
 }
 
 export const StepContext = createContext<StepContextType>({
-  stepStatus: {
-    board: false,
-    switch: false,
-    keyCap: false,
-  },
   currentStep: 'board',
-  updateStepStatus: () => {},
   updateCurrentStep: () => {},
 });
 
@@ -83,23 +69,12 @@ export const KeyColorContext = createContext<KeyColorContextType>({
 
 export function StepContextProvider({ children }: PropsWithChildren) {
   const [currentStep, setCurrentStep] = useState<'board' | 'switch' | 'keyCap'>('board');
-  const [stepStatus, setStepStatus] = useState<StepStatusType>({
-    board: false,
-    switch: false,
-    keyCap: false,
-  });
 
-  const updateStepStatus = useCallback((step: 'board' | 'switch' | 'keyCap', status: boolean) => {
-    setStepStatus((prev) => ({ ...prev, [step]: status }));
-  }, []);
   const updateCurrentStep = useCallback((step: 'board' | 'switch' | 'keyCap') => {
     setCurrentStep(step);
   }, []);
 
-  const contextValue = useMemo(
-    () => ({ stepStatus, currentStep, updateStepStatus, updateCurrentStep }),
-    [stepStatus, currentStep, updateStepStatus, updateCurrentStep],
-  );
+  const contextValue = useMemo(() => ({ currentStep, updateCurrentStep }), [currentStep, updateCurrentStep]);
   return <StepContext.Provider value={contextValue}>{children}</StepContext.Provider>;
 }
 
