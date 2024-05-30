99d5cb6e9f0a3b3650c70f31d06022d8f5829a80
armd482 <armd482@naver.com>
2024-05-30 13:09:43 +0900

    Style: test코드 제거

--- src/app/dw/page.tsx
+++ src/app/dw/page.tsx
@@ -1,23 +1,3 @@
-'use client';
-
-import Modal from '@/components/Modal/Modal';
-import { useState } from 'react';
-
 export default function Page() {
-  const [isOpen, setIsOpen] = useState(false);
-  const onClose = () => {
-    setIsOpen(false);
-  };
-  return (
-    <div>
-      <div>가나다라</div>
-      <div>가나다라</div>
-      <button type="button" onClick={() => setIsOpen(true)}>
-        open
-      </button>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <div style={{ width: '500px', height: '500px', backgroundColor: '#ffffff' }}>test</div>
-      </Modal>
-    </div>
-  );
+  return <div>테스트페이지 dw</div>;
 }
