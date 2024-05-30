3cde0b1c6b4a4afcc4a957c2fdc9896c0df8e5cc
ggjiny <jnss599@gmail.com>
2024-05-30 14:17:57 +0900

    Test: page에서 테스트

--- src/app/yj/page.tsx
+++ src/app/yj/page.tsx
@@ -1,3 +1,18 @@
+'use client';
+
+import { useState } from 'react';
+import Rating from '@/components/Stars/Rating';
+
 export default function Page() {
-  return <div>테스트페이지 yj</div>;
+  const [userRating, setUserRating] = useState(1);
+
+  const handleRatingChange = (rating: number) => {
+    setUserRating(rating);
+  };
+
+  return (
+    <div>
+      <Rating initialRating={userRating} onRatingChange={handleRatingChange} isEditable />
+    </div>
+  );
 }
