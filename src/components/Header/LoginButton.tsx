453599d81dd01912d7c9eea4deb2b44d4cd4543d
minjeong9919 <minjeong9919@naver.com>
2024-06-02 02:28:09 +0900

    Refactor: develop과 머지

---
+++ src/components/Header/LoginButton.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function LoginButton() {
+  const handleClickButton = () => {
+    /* Login modal */
+  };
+
+  return (
+    <button type="button" onClick={handleClickButton}>
+      로그인
+    </button>
+  );
+}
---
+++ src/components/Header/SearchBox.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import SearchIcon from '@/public/svgs/search.svg';
+import classNames from 'classnames/bind';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import styles from './SearchBox.module.scss';
+
+const cn = classNames.bind(styles);
+
+export default function SearchBox() {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const handleSubmitSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    /* 검색 결과 사이트 이동 */
+  };
+  const handleChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
+  return (
+    <form className={cn('wrapper')} onSubmit={handleSubmitSearch}>
+      <input
+        value={searchValue}
+        onChange={handleChangeSearch}
+        className={cn('input-box')}
+        placeholder="검색어를 입력해주세요."
+      />
+      <button type="submit" className={cn('search-button')} aria-label="submit">
+        <SearchIcon width={24} height={24} />
+      </button>
+    </form>
+  );
+}
