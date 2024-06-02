9d5be30db96251af93a89f1b9a79ed59ce6259f7
Young2un <popduddms@gmail.com>
2024-06-02 17:49:45 +0900

    Test: 테스트 페이지

--- src/app/ye/page.tsx
+++ src/app/ye/page.tsx
@@ -1,3 +1,24 @@
+import ProductItem from '@/components/Products/ProductItem';
+
 export default function Page() {
-  return <div>테스트페이지 ye</div>;
+  return (
+    <>
+      <ProductItem
+        id="12"
+        size="sm"
+        imageUrl="https://cdn.imweb.me/thumbnail/20240531/1c4e2517e9231.jpg"
+        title="몬스타기어 닌자 87PRO ALU 스폐셜 에디션 풀알루미늄 커스텀 키보드"
+        price={120000}
+        reviewCount={9}
+      />
+      <ProductItem
+        id="12"
+        size="lg"
+        imageUrl="https://cdn.imweb.me/thumbnail/20240531/1c4e2517e9231.jpg"
+        title="큰애"
+        price={99000}
+        reviewCount={100}
+      />
+    </>
+  );
 }
