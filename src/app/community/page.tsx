7c846d933adc445c119929152263f14847af69d2
minjeong9919 <minjeong9919@naver.com>
2024-06-05 03:03:25 +0900

    Feat: 포스트 글 전체적인 레이아웃 구현

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -3,6 +3,7 @@
 import classNames from 'classnames/bind';
 import { useState } from 'react';
 import { Modal } from '@/components';
+import Button from '@/components/Button/Button';
 import PostCard from './_components/PostCard';
 import styles from './page.module.scss';
 import { COMMUNITY_DATA } from '../mj/CommunityData';
@@ -17,9 +18,29 @@ export default function Page() {
   const handleCustomPostModal = () => {
     setIsPostModalOpen(true);
   };
+
+  const handleWritePostButton = () => {
+    /** 글 작성하기 버튼 */
+  };
   return (
     <div className={cn('container')}>
-      <PostCard cardData={COMMUNITY_DATA} onClick={handleCustomPostModal} />
+      <p className={cn('page-name')}>커뮤니티</p>
+      <div className={cn('filter-write-button-wrapper')}>
+        <div>최신글</div>
+        <Button onClick={handleWritePostButton} width={120} fontSize={14} paddingVertical={8} radius={4}>
+          글 작성하기
+        </Button>
+      </div>
+      <div className={cn('post-wrapper')}>
+        <PostCard cardData={COMMUNITY_DATA} onClick={handleCustomPostModal} />
+        <PostCard cardData={COMMUNITY_DATA} onClick={handleCustomPostModal} />
+        <PostCard cardData={COMMUNITY_DATA} onClick={handleCustomPostModal} />
+        <PostCard cardData={COMMUNITY_DATA} onClick={handleCustomPostModal} />
+        <PostCard cardData={COMMUNITY_DATA} onClick={handleCustomPostModal} />
+        <PostCard cardData={COMMUNITY_DATA} onClick={handleCustomPostModal} />
+        <PostCard cardData={COMMUNITY_DATA} onClick={handleCustomPostModal} />
+      </div>
+
       <Modal isOpen={isPostModalOpen} onClose={handleClosePostModal}>
         modal
       </Modal>
