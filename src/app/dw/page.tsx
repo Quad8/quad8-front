bb0695353b24535108d9764fdd2455ab8c75e6fc
armd482 <armd482@naver.com>
2024-06-05 23:09:57 +0900

    Test: 테스트코드 제거

--- src/app/dw/page.tsx
+++ src/app/dw/page.tsx
@@ -1,14 +1,3 @@
-import Header from '@/components/Header/Header';
-
 export default function Page() {
-  return (
-    <div>
-      <Header />
-      <div style={{ width: '100%', height: '10000vh' }}>
-        testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttestte
-        <br />
-        sttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
-      </div>
-    </div>
-  );
+  return <div>테스트페이지 dw</div>;
 }
