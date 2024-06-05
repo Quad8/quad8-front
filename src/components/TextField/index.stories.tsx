7956f2c16c55a4c9ccdb31b4135209b6b196d227
김보경 <rlaqhrud6263@naver.com>
2024-06-06 02:22:39 +0900

    Feat: TextField story 추가

--- src/components/TextField/index.stories.tsx
+++ src/components/TextField/index.stories.tsx
@@ -9,3 +9,5 @@ type Story = StoryObj<typeof TextField>;
 export const Primary: Story = { args: {} };
 
 export const Medium: Story = { args: { sizeVariant: 'md', label: 'Medium' } };
+
+export const Option: Story = { args: { sizeVariant: 'option', maxLength: 30, minLength: 0, rows: 3 } };
