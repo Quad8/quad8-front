ca9f9eb8ba8dc4aaa2d1d782b1f5b1e5a151d878
김보경 <rlaqhrud6263@naver.com>
2024-06-06 01:44:43 +0900

    Feat: styrybook config 세팅

--- src/components/Dropdown/index.stories.tsx
+++ src/components/Dropdown/index.stories.tsx
@@ -6,4 +6,12 @@ export default meta;
 
 type Story = StoryObj<typeof Dropdown>;
 
-export const Primary: Story = { args: {} };
+export const Primary: Story = { args: { options: ['0', '1', '2'] } };
+
+export const Xsmall: Story = { args: { sizeVariant: 'xs', options: ['0', '1', '2', '직접 입력'] } };
+
+export const Small: Story = { args: { sizeVariant: 'sm', options: ['0', '1', '2'] } };
+
+export const Medium: Story = { args: { sizeVariant: 'md', options: ['0', '1', '2'] } };
+
+export const Large: Story = { args: { sizeVariant: 'lg', options: ['0', '1', '2'] } };
