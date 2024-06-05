f18f887bf830631559964fbd9ec231d8ebb9f25c
minjeong9919 <minjeong9919@naver.com>
2024-06-05 14:49:17 +0900

    Feat: button에 jsDocs을 이용해 props 문서화

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -18,6 +18,19 @@ interface ButtonProps {
   children: ReactNode;
 }
 
+/**
+ * button component documentation
+ * @param {ButtonColorType} [props.backgroundColor] - 버튼에 적용할 디자인
+ * @param {ButtonColorType} [props.hoverColor] - 버튼에 hover 시 적용할 디자인
+ * @param {number} [props.redius] - 버튼의 border-radius 크기 / 4px or 8px
+ * @param {number} [props.width] - 너비의 크기 px 단위 / 'parent-full'은 100%
+ * @param {number} [props.fontSize] - 폰트 크기 px 단위
+ * @param {number} [props.paddingVertical] - 수직 방향 패딩 값
+ * @param {funtion} [props.onClick] - 버튼 클릭 시 실행할 함수
+ * @param {JSX.Element} [props.children] - 버튼 안에 들어갈 내용
+ * @returns {JSX.Element}
+ */
+
 export default function Button({
   backgroundColor = BUTTON_COLOR.BACKGROUND_PRIMARY,
   hoverColor,
