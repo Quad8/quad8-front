541a4fc5f4074f033e58efcc6b8415272abfe773
armd482 <armd482@naver.com>
2024-06-05 22:31:25 +0900

    Design: menu z-index 수정, popins 폰트 url 변경

--- src/app/dw/page.tsx
+++ src/app/dw/page.tsx
@@ -1,3 +1,14 @@
+import Header from '@/components/Header/Header';
+
 export default function Page() {
-  return <div>테스트페이지 dw</div>;
+  return (
+    <div>
+      <Header />
+      <div style={{ width: '100%', height: '10000vh' }}>
+        testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttestte
+        <br />
+        sttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest
+      </div>
+    </div>
+  );
 }
