b04e31f208aa2ce2d81bd7d4e9cb6e82cad44473
armd482 <armd482@naver.com>
2024-06-06 15:29:30 +0900

    Refactor: 커스텀키보드 스타일링 필요없는 코드 제거

--- src/app/custom-keyboard/page.tsx
+++ src/app/custom-keyboard/page.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames/bind';
+import Header from '@/components/Header/Header';
 import styles from './customKeyboard.module.scss';
 import Option from './_components/Option';
 import KeyboardViewer from './_components/KeyboardViewer';
@@ -8,16 +9,19 @@ const cn = classNames.bind(styles);
 
 export default function Page() {
   return (
-    <div className={cn('content-wrapper')}>
-      <KeyboardViewer />
-      <div className={cn('option-wrapper')}>
-        <div className={cn('option-content-wrapper')}>
-          <Option />
-        </div>
-        <div className={cn('button-wrapper')}>
-          <PriceButton />
+    <>
+      <Header />
+      <div className={cn('content-wrapper')}>
+        <KeyboardViewer />
+        <div className={cn('option-wrapper')}>
+          <div className={cn('option-content-wrapper')}>
+            <Option />
+          </div>
+          <div className={cn('button-wrapper')}>
+            <PriceButton />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
