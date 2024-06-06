2768a9cf60cd595c01dffe86ab5479373297464a
minjeong9919 <minjeong9919@naver.com>
2024-06-06 16:30:08 +0900

    Feat: 댓글 입력 창 추가

--- src/app/community/_components/PostCardDetailModal.tsx
+++ src/app/community/_components/PostCardDetailModal.tsx
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 // import { COMMUNITY_DATA_DETAIL } from '@/app/mj/CommunityData';
 import Image from 'next/image';
+import defaultImage from '@/public/images/default.png';
 import contentImage from '@/public/images/myProfile.jpeg';
-// import defaultImage from '@/public/images/defaultProfile.png';
 // import { calculateTimeDifference } from '@/libs/calculateDate';
+import { InputField } from '@/components';
 import styles from './PostCardDetailModal.module.scss';
 import AuthorCard from './AuthorCard';
 import { PostInteractions } from './PostInteractions';
-
+import Comment from './Comment';
 // const COMMENTS = {
 //   profileImage: defaultImage,
 //   nickname: 'nininini',
@@ -17,15 +19,20 @@ import { PostInteractions } from './PostInteractions';
 const cn = classNames.bind(styles);
 
 export default function PostCardDetailModal() {
-  const images = [contentImage, contentImage, contentImage];
+  const images = [contentImage, defaultImage, contentImage];
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+
   return (
     <div className={cn('container')}>
       <div className={cn('images-wrapper')}>
-        <Image src={images[0]} alt='키보드 이미지' className={cn('selected-image')} />
+        <Image src={selectedImage} alt='키보드 이미지' className={cn('selected-image')} />
         {images.length > 1 && (
           <div className={cn('unselected-image-wrapper')}>
-            <Image src={images[0]} alt='키보드 이미지' className={cn('unselected-image')} />
-            <Image src={images[0]} alt='키보드 이미지' className={cn('unselected-image')} />
+            {images.map((image, i) => (
+              <div onClick={() => setSelectedImage(images[i])} key={image.toString()}>
+                <Image src={image} alt='키보드 이미지' className={cn('images')} />
+              </div>
+            ))}
           </div>
         )}
       </div>
@@ -34,6 +41,18 @@ export default function PostCardDetailModal() {
         <AuthorCard nickname='nickname' createdTime='2024.05.25' />
         <p className={cn('content')}>content box</p>
         <PostInteractions goodCount={20} commentCount={20} />
+        <div className={cn('comment-wrapper')}>
+          <Comment />
+          <Comment />
+          <Comment />
+          <Comment />
+          <Comment />
+          <Comment />
+          <Comment />
+        </div>
+        <div className={cn('comment-input')}>
+          <InputField placeholder='댓글을 입력해주세요' />
+        </div>
       </div>
     </div>
   );
