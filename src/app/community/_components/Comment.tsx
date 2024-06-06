4224e3d0419c37e998da302f752198706979e710
minjeong9919 <minjeong9919@naver.com>
2024-06-06 15:09:01 +0900

    Feat: 댓글 컴포넌트 구현

--- src/app/community/_components/Comment.tsx
+++ src/app/community/_components/Comment.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import classNames from 'classnames/bind';
+import Image from 'next/image';
+import defaultImage from '@/public/images/default.png';
 import styles from './Comment.module.scss';
 
 const cn = classNames.bind(styles);
 
-export const comment = () => {
-  return <div className={cn('container')}>comment</div>;
-};
+interface CommentProps {
+  profile?: string;
+}
+
+export default function Comment({ profile }: CommentProps) {
+  return (
+    <div className={cn('container')}>
+      <div className={cn('profile-image')}>
+        <Image src={profile || defaultImage} alt='프로필 이미지' fill />
+      </div>
+      <div className={cn('content-wrapper')}>
+        <div className={cn('user-info')}>
+          <p id={cn('nickname')}>닉네임</p>
+          <p id={cn('time-ago')}>1분전</p>
+        </div>
+        <div className={cn('content')}>댓글 내용이 들어가는곳</div>
+      </div>
+    </div>
+  );
+}
