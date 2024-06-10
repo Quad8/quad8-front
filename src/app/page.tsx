4c4966cea7ed51c68722ac339b3a16c8577ff0c2
Young2un <popduddms@gmail.com>
2024-06-11 02:17:24 +0900

    Feat: 메인페이지 구성중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Header from '@/components/Header/Header';
+import CustomGuide from './_components/CustomGuide';
+import Hero from './_components/Hero';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div>홈페이지 입니다</div>
+      <Hero />
+      <CustomGuide />
     </>
   );
 }
