3cb7d543dd35ce987c5e9359651314f9fe7f2cd5
Young2un <popduddms@gmail.com>
2024-06-11 02:16:32 +0900

    Fix: 뭘해도 안돼서 최선의 대안

--- src/app/_components/heroItem.ts
+++ src/app/_components/heroItem.ts
@@ -4,7 +4,8 @@ import heroItem3 from '@/public/images/heroItem3.png';
 import heroItem4 from '@/public/images/heroItem4.png';
 import heroItem5 from '@/public/images/heroItem5.png';
 import heroItem6 from '@/public/images/heroItem6.png';
+import heroItem7 from '@/public/images/heroItem7.png';
 
-const images = [heroItem1, heroItem2, heroItem3, heroItem4, heroItem5, heroItem6];
+const images = [heroItem1, heroItem2, heroItem3, heroItem4, heroItem5, heroItem6, heroItem7];
 
 export default images;
