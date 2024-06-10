9ff7fd1d7a94a99cda550ea4b22bb5b8964ab73b
Young2un <popduddms@gmail.com>
2024-06-11 02:52:01 +0900

    Fix: 문제 해결로 인한 필요없는 요소 삭제

--- src/app/_components/heroItem.ts
+++ src/app/_components/heroItem.ts
@@ -4,8 +4,7 @@ import heroItem3 from '@/public/images/heroItem3.png';
 import heroItem4 from '@/public/images/heroItem4.png';
 import heroItem5 from '@/public/images/heroItem5.png';
 import heroItem6 from '@/public/images/heroItem6.png';
-import heroItem7 from '@/public/images/heroItem7.png';
 
-const images = [heroItem1, heroItem2, heroItem3, heroItem4, heroItem5, heroItem6, heroItem7];
+const images = [heroItem1, heroItem2, heroItem3, heroItem4, heroItem5, heroItem6];
 
 export default images;
