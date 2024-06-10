6dc5f956208abde0ec3df9a6d09079222acb23de
minjeong9919 <minjeong9919@naver.com>
2024-06-10 11:55:14 +0900

    Feat: hover 시 스토리북 작성

--- src/components/Button/Background.stories.tsx
+++ src/components/Button/Background.stories.tsx
@@ -17,3 +17,10 @@ export const Primary60Background: Story = { args: { backgroundColor: 'background
 export const GrayBorder: Story = { args: { backgroundColor: 'outline-gray-40' } };
 export const PrimaryBorder: Story = { args: { backgroundColor: 'outline-primary' } };
 export const Primary60Border: Story = { args: { backgroundColor: 'outline-primary-60' } };
+
+export const GrayBackgroundHover: Story = { args: { hoverColor: 'background-gray-40' } };
+export const PrimaryBackgroundHover: Story = { args: { hoverColor: 'background-primary' } };
+export const Primary60BackgroundHover: Story = { args: { hoverColor: 'background-primary-60' } };
+export const GrayBorderHover: Story = { args: { hoverColor: 'outline-gray-40' } };
+export const PrimaryBorderHover: Story = { args: { hoverColor: 'outline-primary' } };
+export const Primary60BorderHover: Story = { args: { hoverColor: 'outline-primary-60' } };
