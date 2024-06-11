a4eca4b4ee2962975f95522ae4ee2bf1c58d2eba
minjeong9919 <minjeong9919@naver.com>
2024-06-11 17:09:07 +0900

    Feat: 로그인 버튼 및 Oauth 추가

--- src/app/sign-in/_component/SignInModal.tsx
+++ src/app/sign-in/_component/SignInModal.tsx
@@ -1,27 +1,48 @@
 import classNames from 'classnames/bind';
 import { InputField } from '@/components';
+import Button from '@/components/Button/Button';
+import GitIcon from '@/public/svgs/GitHub.svg';
+import GoogleIcon from '@/public/svgs/Google.svg';
+import KakaoIcon from '@/public/svgs/kakaotalk.svg';
 import styles from './SigninModal.module.scss';
 
 const cn = classNames.bind(styles);
+
+const AUTH_SECTION = ['아이디 찾기', '비밀번호 찾기', '회원가입'];
 
 export default function SignInModal() {
   return (
     <div className={cn('container')}>
       <h1 className={cn('title')}>로그인</h1>
-      <div className={cn('input-div')}>
-        <InputField label='이메일' placeholder='이메일을 입력해주세요' sizeVariant='lg' />
+      <div className={cn('input-wrapper')}>
+        <InputField label='이메일' placeholder='이메일을 입력해주세요' sizeVariant='md' labelSize='sm' />
         <InputField
           label='비밀번호'
           placeholder='비밀번호를 입력해주세요'
-          sizeVariant='lg'
+          sizeVariant='md'
+          labelSize='sm'
           type='password'
           suffixIcon='eye'
         />
       </div>
       <div className={cn('auth-section')}>
-        <p>아이디 찾기</p>
-        <p>비밀번호 찾기</p>
-        <p>회원가입</p>
+        {AUTH_SECTION.map((text, i) => (
+          <>
+            <p key={text}>{text}</p>
+            {i === 2 || <div className={cn('bar')}>|</div>}
+          </>
+        ))}
+      </div>
+      <Button className={cn('button')} fontSize={24}>
+        로그인
+      </Button>
+      <div className={cn('o-auth-wrapper')}>
+        <p>간편 로그인 하기</p>
+        <div className={cn('icons')}>
+          <GitIcon />
+          <GoogleIcon />
+          <KakaoIcon />
+        </div>
       </div>
     </div>
   );
