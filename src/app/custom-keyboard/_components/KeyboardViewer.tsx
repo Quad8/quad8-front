705e40c32916e2c8aacb3fa3bc7c9ec2de21b916
armd482 <armd482@naver.com>
2024-06-12 03:08:34 +0900

    Feat: 커스텀 키보드 페이지 canvas 로딩 처리

--- src/app/custom-keyboard/_components/KeyboardViewer.tsx
+++ src/app/custom-keyboard/_components/KeyboardViewer.tsx
@@ -2,13 +2,14 @@
 
 import { Canvas } from '@react-three/fiber';
 import { Environment, OrbitControls } from '@react-three/drei';
-import { Suspense, useContext } from 'react';
+import { Suspense, useContext, useState } from 'react';
 import classNames from 'classnames/bind';
 import { KeyColorContext, StepContext } from '@/context/customKeyboardContext';
 import { Vector3 } from 'three';
 import styles from './KeyboardViewer.module.scss';
 import Keyboard from './Keyboard';
 import Step from './Step';
+import CanvasLoading from './CanvasLoading';
 
 const cn = classNames.bind(styles);
 
@@ -23,6 +24,7 @@ const GL = {
 };
 
 export default function KeyboardViewer() {
+  const [isLoaded, setIsLoaded] = useState(false);
   const { updateFocusKey } = useContext(KeyColorContext);
   const { canvasRef, controlRef } = useContext(StepContext);
 
@@ -32,11 +34,16 @@ export default function KeyboardViewer() {
 
   return (
     <div className={cn('wrapper')}>
-      <div className={cn('option-wrapper')}>
-        <Step />
-      </div>
-      <Suspense>
-        <Canvas camera={CAMERA} className={cn('canvas')} gl={GL} onPointerMissed={handleClickCanvas} ref={canvasRef}>
+      <Suspense fallback={<CanvasLoading />}>
+        <div className={cn('option-wrapper')}>{isLoaded && <Step />}</div>
+        <Canvas
+          camera={CAMERA}
+          className={cn('canvas')}
+          gl={GL}
+          onPointerMissed={handleClickCanvas}
+          ref={canvasRef}
+          onCreated={() => setIsLoaded(true)}
+        >
           <Keyboard />
           <Environment preset='city' />
           <OrbitControls ref={controlRef} />
