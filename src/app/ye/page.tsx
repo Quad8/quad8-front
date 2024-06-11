8ed777e99423fd28b9d2c93d109b9014248576b3
Young2un <popduddms@gmail.com>
2024-06-11 16:05:19 +0900

    Fix: 빌드문제 해결

--- src/app/ye/page.tsx
+++ src/app/ye/page.tsx
@@ -1,5 +1,13 @@
 import Pagination from '@/components/Pagination/Pagination';
+import { Suspense } from 'react';
 
 export default function Page() {
-  return <Pagination count={161} limit={16} />;
+  return (
+    <div>
+      <Suspense fallback={<div>로딩 중...</div>}>
+        <h1>페이지 콘텐츠</h1>
+        <Pagination count={100} limit={16} />
+      </Suspense>
+    </div>
+  );
 }
