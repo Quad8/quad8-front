e1dbd2d18fdf4cb0ba4dac7abea9eeca2a03fded
armd482 <armd482@naver.com>
2024-06-12 02:40:48 +0900

    Feat: 커스텀 키보드 페이지 장바구니 모달 옵션 카드 컴포넌트 구현

---
+++ src/app/custom-keyboard/_components/CartModalOptionCard.tsx
@@ -0,0 +1,92 @@
+import classNames from 'classnames/bind';
+import Image, { StaticImageData } from 'next/image';
+import { MouseEvent, useEffect, useRef, useState } from 'react';
+import styles from './CartModalOptionCard.module.scss';
+
+const cn = classNames.bind(styles);
+
+interface CartModalOptionCardProps {
+  name: string;
+  option1: string;
+  option2?: string;
+  buttonType: 'edit' | 'delete';
+  count?: number;
+  price: number;
+  imageSrc: string | StaticImageData;
+  buttonOnClick: (e: MouseEvent<HTMLDivElement>) => void;
+}
+
+export default function CartModalOptionCard({
+  name,
+  option1,
+  option2,
+  buttonType,
+  count,
+  price,
+  imageSrc,
+  buttonOnClick,
+}: CartModalOptionCardProps) {
+  const BUTTON_TYPE = {
+    edit: '주문 수정',
+    delete: '주문 삭제',
+  };
+  const tooltipRef = useRef<HTMLDivElement>(null);
+  const targetRef = useRef<HTMLDivElement>(null);
+  const [isHover, setIsHover] = useState(false);
+
+  useEffect(() => {
+    if (isHover && tooltipRef.current && targetRef.current) {
+      const viewportTop = targetRef.current.getBoundingClientRect().top + 20;
+      const tooltipHeight = tooltipRef.current.clientHeight;
+      const viewportHeight = window.innerHeight;
+
+      if (viewportHeight < viewportTop + tooltipHeight) {
+        tooltipRef.current.style.bottom = '0px';
+        tooltipRef.current.style.top = 'auto';
+      } else {
+        tooltipRef.current.style.top = `${viewportTop}px`;
+        tooltipRef.current.style.bottom = 'auto';
+      }
+    }
+  }, [isHover]);
+
+  return (
+    <div className={cn('wrapper')}>
+      <Image src={imageSrc} width={104} height={104} alt='장바구니 이미지' className={cn('image')} />
+      <div className={cn('content-wrapper')}>
+        <div className={cn('title-option-wrapper')}>
+          <div className={cn('title-wrapper')}>
+            <p className={cn('title')}>{name}</p>
+            <div className={cn('button')} onClick={(e) => buttonOnClick(e)}>
+              {BUTTON_TYPE[buttonType]}
+            </div>
+          </div>
+          <div className={cn('option-wrapper')}>
+            <div className={cn('option')}>{option1}</div>
+            <div
+              className={cn('second-option-wrapper')}
+              onMouseEnter={() => setIsHover(true)}
+              onMouseLeave={() => setIsHover(false)}
+              ref={targetRef}
+            >
+              <div className={cn('option')}>{option2}</div>
+              <div className={cn('tooltip-wrapper', { 'tooltip-visible': isHover })} ref={tooltipRef}>
+                <div
+                  className={cn('tooltip')}
+                  onMouseEnter={() => setIsHover(true)}
+                  onMouseLeave={() => setIsHover(false)}
+                >
+                  {option2}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={cn('count-price-wrapper')}>
+          {count ? <div className={cn('count')}>{count}개</div> : <div />}
+          <div className={cn('price')}>{price.toLocaleString()}원</div>
+        </div>
+      </div>
+    </div>
+  );
+}
