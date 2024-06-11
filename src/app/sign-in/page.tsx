8e053d00103e8111cccc3e72430300c82721e1ff
minjeong9919 <minjeong9919@naver.com>
2024-06-12 00:58:50 +0900

    Feat: 회원가입 모달창 인풋창 삽입

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -6,19 +6,34 @@ import { useState } from 'react';
 import { Modal } from '@/components';
 import styles from './page.module.scss';
 import SignInModal from './_component/SignInModal';
+import SignupModal from './_component/SignupModal';
 
 const cn = classNames.bind(styles);
 
 export default function Page() {
   const [isOpenModal, setIsOpenModal] = useState(false);
+  const [modalType, setOpenModalType] = useState('');
+
   const handleCloseModal = () => {
     setIsOpenModal(false);
   };
+
+  const handleSignin = () => {
+    setIsOpenModal(true);
+    setOpenModalType('signin');
+  };
+
+  const handleSignup = () => {
+    setIsOpenModal(true);
+    setOpenModalType('signup');
+  };
+
   return (
     <div className={cn('container')}>
-      <Button onClick={() => setIsOpenModal(true)}> 로그인 모달창</Button>
+      <Button onClick={handleSignin}> 로그인 모달창</Button>
+      <Button onClick={handleSignup}> 회원가입 모달창</Button>
       <Modal isOpen={isOpenModal} onClose={handleCloseModal}>
-        <SignInModal />
+        {modalType === 'signin' ? <SignInModal /> : <SignupModal />}
       </Modal>
     </div>
   );
