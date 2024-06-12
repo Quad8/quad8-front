25e6ff8e11ecafbf63bd09e9d205f6a13b12a487
minjeong9919 <minjeong9919@naver.com>
2024-06-13 00:15:33 +0900

    Refactor: test 페이지에서 header 제거

--- src/app/(test)/mj/page.tsx
+++ src/app/(test)/mj/page.tsx
@@ -1,5 +1,4 @@
 import classNames from 'classnames/bind';
-import Header from '@/components/Header/Header';
 import ProfileImage from '@/components/ProfileImage/ProfileImage';
 import styles from './page.module.scss';
 import ReviewModalTest from './ReviewModalTest';
@@ -9,7 +8,6 @@ const cn = classNames.bind(styles);
 export default function Page() {
   return (
     <div className={cn('container')}>
-      <Header />
       <ProfileImage isEditable width={139} height={139} profileImage={null} />
       <ReviewModalTest />
     </div>
