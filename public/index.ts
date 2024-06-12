381ef682bef381da082dce209168eca002b4a8b6
armd482 <armd482@naver.com>
2024-06-12 23:09:14 +0900

    Style: 커스텀 키보드 페이지 color 태그 컴포넌트 커서 스타일 추가

--- public/index.ts
+++ public/index.ts
@@ -3,7 +3,6 @@ export { default as EyeOffIcon } from './svgs/eyeOff.svg';
 export { default as EyeOnIcon } from './svgs/eyeOn.svg';
 export { default as LogoIcon } from './svgs/logo.svg';
 export { default as SearchIcon } from './svgs/search.svg';
-export { default as ArrowDownIcon } from './svgs/arrowDown.svg';
 export { default as blueSwitchImg } from './images/blueSwitch.png';
 export { default as redSwitchImg } from './images/redSwitch.png';
 export { default as brownSwitchImg } from './images/brownSwitch.png';
