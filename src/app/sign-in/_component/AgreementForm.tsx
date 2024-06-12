11b4dc4dbff6715412d04725623054edefa838be
minjeong9919 <minjeong9919@naver.com>
2024-06-12 11:12:01 +0900

    Feat: 약관 체크 유무에 따라 모두 동의 약관이 자동으로 변하도록 수정

--- src/app/sign-in/_component/AgreementForm.tsx
+++ src/app/sign-in/_component/AgreementForm.tsx
@@ -1,25 +1,36 @@
 import classNames from 'classnames/bind';
 import CheckIcon from '@/public/svgs/check';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import RightArrow from '@/public/svgs/caretRight.svg';
 import styles from './AgreementForm.module.scss';
 
 const cn = classNames.bind(styles);
 
 export function AgreementForm() {
-  const [iconColors, setIconColors] = useState<string[]>(['#A5A5A5', '#A5A5A5', '#A5A5A5']);
+  const NOT_CHECKED = '#A5A5A5';
+  const CHECKED = '#4968f6';
+
+  const [iconColors, setIconColors] = useState<string[]>([NOT_CHECKED, NOT_CHECKED, NOT_CHECKED]);
 
   const changeColor = (i: number) => {
     const updatedColors = [...iconColors];
-    updatedColors[i] = updatedColors[i] === '#A5A5A5' ? '#4968f6' : '#A5A5A5';
+    updatedColors[i] = updatedColors[i] === NOT_CHECKED ? CHECKED : NOT_CHECKED;
     setIconColors(updatedColors);
   };
 
   const handleAllCheck = () => {
-    const allChecked = iconColors.every((color) => color === '#4968f6');
-    const newColor = allChecked ? '#A5A5A5' : '#4968f6';
+    const allChecked = iconColors.every((color) => color === CHECKED);
+    const newColor = allChecked ? NOT_CHECKED : CHECKED;
     setIconColors([newColor, newColor, newColor]);
   };
+
+  useEffect(() => {
+    const isAllNoCheck = iconColors[0] === CHECKED && iconColors[1] === NOT_CHECKED && iconColors[2] === NOT_CHECKED;
+    const isAllCheck = iconColors[0] === NOT_CHECKED && iconColors[1] === CHECKED && iconColors[2] === CHECKED;
+
+    if (isAllNoCheck) setIconColors([NOT_CHECKED, NOT_CHECKED, NOT_CHECKED]);
+    if (isAllCheck) setIconColors([CHECKED, CHECKED, CHECKED]);
+  }, [iconColors]);
 
   return (
     <div className={cn('container')}>
@@ -29,14 +40,14 @@ export function AgreementForm() {
       </div>
       <div className={cn('content-wrapper')}>
         <div className={cn('content')}>
-          <CheckIcon onClick={() => changeColor(1)} color={iconColors[1]} />
+          <CheckIcon onClick={() => changeColor(1)} color={iconColors[1]} width={20} height={20} />
           <h3>서비스 이용 약관 동의</h3>
-          <RightArrow className={cn('right-arrow')} />
+          <RightArrow className={cn('right-arrow')} stroke={NOT_CHECKED} />
         </div>
         <div className={cn('content')}>
-          <CheckIcon onClick={() => changeColor(2)} color={iconColors[2]} />
+          <CheckIcon onClick={() => changeColor(2)} color={iconColors[2]} width={20} height={20} />
           <h3>서비스 이용 약관 동의</h3>
-          <RightArrow className={cn('right-arrow')} />
+          <RightArrow className={cn('right-arrow')} stroke={NOT_CHECKED} />
         </div>
       </div>
     </div>
