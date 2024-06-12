2118a77b19c254272c21ef6d5d161a14b0212405
Young2un <popduddms@gmail.com>
2024-06-12 16:46:28 +0900

    Fix: 타입안적어서 빌드오류 난거 해결용

--- src/app/ye/shop/[category]/page.tsx
+++ src/app/ye/shop/[category]/page.tsx
@@ -1,4 +1,4 @@
-export default function page({ params }) {
+export default function Page({ params }: { params: { category: string } }) {
   const { category } = params;
   return <div>{category} 리스트 페이지</div>;
 }
