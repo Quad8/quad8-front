824674cbc6092ec83e522c96976ea37f7341ffbe
Young2un <popduddms@gmail.com>
2024-06-13 18:21:54 +0900

    Fix: 폴더 구조 변경

---
+++ src/app/shop/_components/ProductList.tsx
@@ -0,0 +1,21 @@
+import { Product } from '@/api/getProductList';
+import ProductItem from '@/components/Products/ProductItem';
+import classNames from 'classnames/bind';
+import styles from './ProductList.module.scss';
+
+const cn = classNames.bind(styles);
+
+interface ProductListProps {
+  content: Product[];
+  size: 'lg' | 'sm';
+}
+
+export default function ProductList({ content, size }: ProductListProps) {
+  return (
+    <ul className={cn('product-list')}>
+      {content.map((product) => (
+        <ProductItem key={product.id} size={size} {...product} />
+      ))}
+    </ul>
+  );
+}
