2beec792e2eddf579b723ff6c749c6e7198e3e6f
Young2un <popduddms@gmail.com>
2024-06-14 00:21:27 +0900

    Chore: env추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_KEYDEUK_API_BASE_URL: process.env.NEXT_PUBLIC_KEYDEUK_API_BASE_URL,
+  },
 
   images: {
     remotePatterns: [
