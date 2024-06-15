cd925c4d62d8c206c4035467c6a323f8d29c3847
Young2un <popduddms@gmail.com>
2024-06-16 03:16:28 +0900

    Refactor: 페이지네이션 수정중

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,36 +1,33 @@
 'use client';
 
-import calculateStartPageNum from '@/libs/calculatePage';
+import { ProductDataResponse } from '@/api/getProductList';
 import CaretLeftIcon from '@/public/svgs/caretLeft.svg';
 import CaretRightIcon from '@/public/svgs/caretRight.svg';
 import classNames from 'classnames/bind';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import styles from './Pagination.module.scss';
 
-interface PaginationProps {
-  count: number;
-  limit: number;
-}
+type PaginationProps = Omit<ProductDataResponse, 'content' | 'size'>;
 
 const cn = classNames.bind(styles);
 
-const MAX_PAGE_LENGTH = 6;
-
-export default function Pagination({ count, limit }: PaginationProps) {
+export default function Pagination({
+  totalElements,
+  totalPages: totalPageCount,
+  number: currentPage,
+  first,
+  last,
+}: PaginationProps) {
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const currentPage = Math.max(1, parseInt(searchParams.get('page') || '1', 10));
-  const totalPageCount = Math.ceil(count / limit);
 
-  if (!count) {
+  if (!totalElements) {
     return null;
   }
 
-  const startPageNum = calculateStartPageNum(currentPage, MAX_PAGE_LENGTH);
-
   const handlePageChange = (newPage: number) => {
-    if (newPage < 1 || newPage > totalPageCount) {
+    if (newPage < 0 || newPage > totalPageCount) {
       return;
     }
     const params = new URLSearchParams(searchParams.toString());
@@ -44,21 +41,18 @@ export default function Pagination({ count, limit }: PaginationProps) {
         type='button'
         onClick={() => handlePageChange(currentPage - 1)}
         className={cn('arrow-button')}
-        disabled={currentPage === 1}
+        disabled={first}
         aria-label='이전 페이지'
       >
-        <CaretLeftIcon stroke={currentPage === 1 ? '#B8B8B8' : '#4968f6'} />
+        <CaretLeftIcon stroke={first ? '#B8B8B8' : '#4968f6'} />
       </button>
       <ul className={cn('pagination-list')}>
-        {Array.from(
-          { length: Math.min(MAX_PAGE_LENGTH, totalPageCount - startPageNum + 1) },
-          (_, i) => startPageNum + i,
-        ).map((pageNum) => (
+        {Array.from({ length: totalPageCount }, (_, i) => i + 1).map((pageNum) => (
           <li key={pageNum} className={cn('pagination-item')}>
-            {currentPage === pageNum ? (
+            {currentPage + 1 === pageNum ? (
               <span className={cn('selected-page')}>{pageNum}</span>
             ) : (
-              <button type='button' onClick={() => handlePageChange(pageNum)} aria-label={`Page ${pageNum}`}>
+              <button type='button' onClick={() => handlePageChange(pageNum - 1)} aria-label={`Page ${pageNum}`}>
                 {pageNum}
               </button>
             )}
@@ -69,10 +63,10 @@ export default function Pagination({ count, limit }: PaginationProps) {
         type='button'
         onClick={() => handlePageChange(currentPage + 1)}
         className={cn('arrow-button')}
-        disabled={currentPage === totalPageCount}
+        disabled={last}
         aria-label='다음 페이지'
       >
-        <CaretRightIcon stroke={currentPage === totalPageCount ? '#B8B8B8' : '#4968f6'} />
+        <CaretRightIcon stroke={last ? '#B8B8B8' : '#4968f6'} />
       </button>
     </div>
   );
