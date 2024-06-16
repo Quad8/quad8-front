213450be8faaa0064927613b4d005942793ab091
ggjiny <jnss599@gmail.com>
2024-06-16 23:33:40 +0900

    Feat: 상품 상제 get api 구현

---
+++ src/api/getProductDetail.ts
@@ -0,0 +1,17 @@
+import { ProductType } from '@/types/ProductTypes';
+
+export async function getProductDetail(productId: string): Promise<ProductType> {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_KEYDEUK_API_BASE_URL}/api/v1/product/get-detail-info/${productId}`,
+      {
+        cache: 'no-store',
+      },
+    );
+    const result = await res.json();
+
+    return result.data;
+  } catch {
+    throw new Error(`상품을 조회할 수 없습니다. `);
+  }
+}
