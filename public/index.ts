6f7607af371479a1b41d9204b02624486e6b7fe1
ggjiny <jnss599@gmail.com>
2024-06-17 04:33:19 +0900

    Feat: 드롭다운 옵션과 하단 버튼을 갖는 컴포넌트 구현

--- public/index.ts
+++ public/index.ts
@@ -1,10 +1,13 @@
+export { default as blackSwitchImg } from './images/blackSwitch.png';
+export { default as blueSwitchImg } from './images/blueSwitch.png';
+export { default as brownSwitchImg } from './images/brownSwitch.png';
+export { default as redSwitchImg } from './images/redSwitch.png';
 export { default as ChevronIcon } from './svgs/chevron.svg';
+export { default as DeleteIcon } from './svgs/delete.svg';
 export { default as EyeOffIcon } from './svgs/eyeOff.svg';
 export { default as EyeOnIcon } from './svgs/eyeOn.svg';
 export { default as LogoIcon } from './svgs/logo.svg';
+export { default as MinusIcon } from './svgs/minusCircle.svg';
+export { default as PlusIcon } from './svgs/plusCircle.svg';
 export { default as SearchIcon } from './svgs/search.svg';
-export { default as blueSwitchImg } from './images/blueSwitch.png';
-export { default as redSwitchImg } from './images/redSwitch.png';
-export { default as brownSwitchImg } from './images/brownSwitch.png';
-export { default as blackSwitchImg } from './images/blackSwitch.png';
 export { default as UserIcon } from './svgs/user.svg';
---
+++ src/app/(test)/yj/_components/OptionWithButtons.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { Button, Dropdown } from '@/components';
+import { DeleteIcon, MinusIcon, PlusIcon } from '@/public/index';
+import { OptionTypes } from '@/types/ProductTypes';
+import classNames from 'classnames/bind';
+import { useState } from 'react';
+import styles from './ProductDetail.module.scss';
+
+const cn = classNames.bind(styles);
+
+interface OptionCountProps {
+  optionText?: string;
+  price?: number;
+}
+
+interface OptionWithButtonProps {
+  optionList: OptionTypes[];
+  price: number;
+}
+
+type OperationType = 'plus' | 'minus';
+
+function OptionContainer({ optionText, price }: OptionCountProps) {
+  const [count, setCount] = useState<number>(1);
+  const [isOptionSelected, SetIsOptionSelected] = useState<boolean>(true);
+
+  const handleClickIcon = (operation: OperationType) => {
+    if (operation === 'minus' && count > 1) {
+      setCount((prev) => prev - 1);
+    } else if (operation === 'plus') {
+      setCount((prev) => prev + 1);
+    }
+  };
+
+  const handleClickDeleteIcon = () => {
+    setCount(0);
+    SetIsOptionSelected(false);
+  };
+
+  const renderQuantitySelector = () => {
+    return (
+      <div className={cn('count-icons')}>
+        <MinusIcon className={cn('icon', { 'gray-icon': count === 1 })} onClick={() => handleClickIcon('minus')} />
+        <span>{count}</span>
+        <PlusIcon className={cn('icon')} onClick={() => handleClickIcon('plus')} />
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <div className={cn({ 'option-box': optionText, hidden: !isOptionSelected })}>
+        {optionText ? (
+          <>
+            <h3 className={cn('option-text')}>{optionText}</h3>
+            {renderQuantitySelector()}
+            <h3 className={cn('option-price')}>{price?.toLocaleString()}원</h3>
+            <DeleteIcon className={cn('delete-icon')} onClick={handleClickDeleteIcon} />
+          </>
+        ) : (
+          renderQuantitySelector()
+        )}
+      </div>
+      <div className={cn('total-price-box')}>
+        <h3>총 금액</h3>
+        <h1>
+          <span>{price && (price * count).toLocaleString()}</span>원
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default function OptionWithButton({ optionList, price }: OptionWithButtonProps) {
+  const optionNames = optionList?.map((option) => option.optionName);
+
+  return (
+    <>
+      <div className={cn('option-section')}>
+        <h2 className={cn('explain-title')}>상품 선택</h2>
+        {optionList && <Dropdown options={optionNames} placeholder='스위치 (필수)' />}
+        <OptionContainer price={price} optionText='리니어' />
+      </div>
+      <div className={cn('button-section')}>
+        <Button>장바구니</Button>
+        <Button>구매하기</Button>
+      </div>
+    </>
+  );
+}
