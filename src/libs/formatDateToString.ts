6a273395836127ec7dc22f29d9d226d5f66554bc
minjeong9919 <minjeong9919@naver.com>
2024-06-17 13:39:55 +0900

    Style: 피드백 반영

--- src/libs/formatDateToString.ts
+++ src/libs/formatDateToString.ts
@@ -6,8 +6,8 @@
 
 export const formatDateToString = (date: Date) => {
   const year = date.getFullYear();
-  const month = `${date.getMonth() < 9 ? '0' : ''}${date.getMonth() + 1}`;
-  const day = `${date.getDate() < 9 ? '0' : ''}${date.getDate()}`;
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
   const dateString = `${year}.${month}.${day}`;
   return dateString;
 };
