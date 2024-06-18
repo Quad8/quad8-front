024dedcafce5c01ade05e89cc48ba8596d1846ad
minjeong9919 <minjeong9919@naver.com>
2024-06-19 02:23:04 +0900

    Refactor: api 네트워크 오류, 응답 상태 처리

--- src/api/authAPI.ts
+++ src/api/authAPI.ts
@@ -4,16 +4,30 @@ const BASE_URL = process.env.NEXT_PUBLIC_KEYDEUK_API_BASE_URL;
 
 export const checkEmailDuplication = async (emailValue: string) => {
   const url = `${BASE_URL}/api/v1/users/check/email?email=${emailValue}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('네트워크 오류');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('이메일 중복 api 요청 실패!');
+  }
 };
 
 export const checkNicknameDuplication = async (nickname: string) => {
   const url = `${BASE_URL}/api/v1/users/check/nickname?nickname=${nickname}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('네트워크 오류');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('닉네임 중복 api 요청 실패!');
+  }
 };
 
 export const postSignup = async (formData: FormData) => {
