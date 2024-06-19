51b93edd0213f231b2fcd1c6d3e6f5ec7971b562
Young2un <popduddms@gmail.com>
2024-06-20 03:16:28 +0900

    Feat: 카테고리 리스트 구현

--- src/api/productAPI.ts
+++ src/api/productAPI.ts
@@ -1,4 +1,4 @@
-import { ProductListResponse, ProductParams } from '@/types/ProductItem';
+import type { GetCategoryListParams, ProductListResponse, ProductParams } from '@/types/ProductItem';
 
 const baseURL = process.env.NEXT_PUBLIC_KEYDEUK_API_BASE_URL;
 
@@ -13,25 +13,54 @@ export async function getAllProductList({ sort, page, size }: ProductParams): Pr
 
   return rawData;
 }
-export async function getCategoryProductList({ keyword, sort, page, size, company, switchType, minPrice, maxPrice }) {
-  const queryParams = new URLSearchParams({
-    keyword: encodeURIComponent(keyword),
-    sort: encodeURIComponent(sort),
-    page: encodeURIComponent(page),
-    size: encodeURIComponent(size),
-    ...(company && { company: encodeURIComponent(company) }),
-    ...(switchType && { switchType: encodeURIComponent(switchType) }),
-    ...(minPrice && { minPrice: encodeURIComponent(minPrice) }),
-    ...(maxPrice && { maxPrice: encodeURIComponent(maxPrice) }),
-  }).toString();
 
-  const response = await fetch(`${baseURL}/api/v1/product/get/category-list?${queryParams}`);
+export async function getCategoryProductList({
+  keyword,
+  sort,
+  page,
+  size,
+  company,
+  switchType,
+  minPrice,
+  maxPrice,
+}: GetCategoryListParams): Promise<ProductListResponse> {
+  try {
+    const queryParams = new URLSearchParams({
+      keyword: encodeURIComponent(keyword),
+      sort: encodeURIComponent(sort as string),
+      page: encodeURIComponent(page as string),
+      size: encodeURIComponent(size),
+      ...(company && { company: encodeURIComponent(company as string) }),
+      ...(switchType && { switchType: encodeURIComponent(switchType as string) }),
+      ...(minPrice && { minPrice: encodeURIComponent(minPrice as string) }),
+      ...(maxPrice && { maxPrice: encodeURIComponent(maxPrice as string) }),
+    }).toString();
 
-  if (!response.ok) {
-    throw new Error(`Failed to load data for ${keyword}: ${response.status} ${response.statusText}`);
+    const response = await fetch(`${baseURL}/api/v1/product/get/category-list?${queryParams}`);
+
+    if (!response.ok) {
+      const errorDetails = await response.text();
+      throw new Error(`${keyword} 카테고리 : ${response.status} ${response.statusText} - ${errorDetails}`);
+    }
+
+    const rawData: ProductListResponse = await response.json();
+
+    return rawData;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+
+    return {
+      data: {
+        totalPages: 0,
+        totalElements: 0,
+        size: 0,
+        content: [],
+        number: 0,
+        first: false,
+        last: false,
+      },
+    };
   }
-
-  const rawData = await response.json();
-
-  return rawData;
 }
