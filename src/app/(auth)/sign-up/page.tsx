8b46755e101fa76bacc46301154d9aa7154b1f44
minjeong9919 <minjeong9919@naver.com>
2024-06-20 21:53:00 +0900

    Test: push 중 오류 해결 테스트 1

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.scss';
 
 const cn = classNames.bind(styles);
 
-export default function Page() {
+export default function SignUpPage() {
   return (
     <div className={cn('container')}>
       <div className={cn('content-wrapper')}>
