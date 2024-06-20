53ad9348cd59e9165526f14328fd95c500fd9fcb
김보경 <rlaqhrud6263@naver.com>
2024-06-20 11:45:58 +0900

    Feat: Dropdown컴포넌트 suffix옵셔널 추가

--- src/app/my-info/(account)/addresses/page.tsx
+++ src/app/my-info/(account)/addresses/page.tsx
@@ -1,5 +1,11 @@
+import { Dropdown } from '@/components';
 import { Address } from './_components';
 
 export default function AddressesPage() {
-  return <Address />;
+  return (
+    <>
+      <Address />
+      <Dropdown options={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]} sizeVariant='xs' isDate />
+    </>
+  );
 }
