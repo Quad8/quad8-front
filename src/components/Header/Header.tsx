a11b8a8c98da4f72ba7360aa69ff0fb6f50b0cca
김보경 <rlaqhrud6263@naver.com>
2024-06-21 19:32:09 +0900

    Style: header bg 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -27,32 +27,34 @@ export default function Header() {
   const users = userData?.data ?? null;
 
   return (
-    <header className={cn('wrapper', { black: isBlack })}>
-      <div className={cn('right-wrapper')}>
-        <Link className={cn('logo')} href={ROUTER.MAIN}>
-          <LogoIcon width={131} height={24} />
-        </Link>
-        <div className={cn('button-wrapper')}>
-          <Link
-            href={ROUTER.CUSTOM_KEYBOARD}
-            className={cn('button', { 'current-page': pathname === ROUTER.CUSTOM_KEYBOARD })}
-          >
-            커스텀 키보드 만들기
+    <header className={cn('header', { 'bg-black': isBlack })}>
+      <div className={cn('wrapper', { black: isBlack })}>
+        <div className={cn('right-wrapper')}>
+          <Link className={cn('logo')} href={ROUTER.MAIN}>
+            <LogoIcon width={131} height={24} />
           </Link>
-          <ShopButton pathname={pathname} />
-          <Link href={ROUTER.COMMUNITY} className={cn('button', { 'current-page': pathname === ROUTER.COMMUNITY })}>
-            커뮤니티
-          </Link>
+          <div className={cn('button-wrapper')}>
+            <Link
+              href={ROUTER.CUSTOM_KEYBOARD}
+              className={cn('button', { 'current-page': pathname === ROUTER.CUSTOM_KEYBOARD })}
+            >
+              커스텀 키보드 만들기
+            </Link>
+            <ShopButton pathname={pathname} />
+            <Link href={ROUTER.COMMUNITY} className={cn('button', { 'current-page': pathname === ROUTER.COMMUNITY })}>
+              커뮤니티
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className={cn('left-wrapper')}>
-        <SearchBox isBlack={isBlack} />
-        <div className={cn('status-wrapper')}>
-          {!users ? <LoginButton /> : <LogoutButton />}
-          <Link href={ROUTER.MY_PAGE.MY_INFO} className={cn('user-icon')}>
-            <UserIcon width={31} height={31} className={cn(isBlack ? 'user-black' : 'user-white')} />
-          </Link>
-          <CartButton cartCount={cartCount} isBlack={isBlack} />
+        <div className={cn('left-wrapper')}>
+          <SearchBox isBlack={isBlack} />
+          <div className={cn('status-wrapper')}>
+            {!users ? <LoginButton /> : <LogoutButton />}
+            <Link href={ROUTER.MY_PAGE.MY_INFO} className={cn('user-icon')}>
+              <UserIcon width={31} height={31} className={cn(isBlack ? 'user-black' : 'user-white')} />
+            </Link>
+            <CartButton cartCount={cartCount} isBlack={isBlack} />
+          </div>
         </div>
       </div>
     </header>
