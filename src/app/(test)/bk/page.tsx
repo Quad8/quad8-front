d6d3495e890cec3808eb5d4ba78947712c8ef34e
김보경 <rlaqhrud6263@naver.com>
2024-06-21 22:40:55 +0900

    Refactor: Dropdown CSS 수정

--- src/app/(test)/bk/page.tsx
+++ src/app/(test)/bk/page.tsx
@@ -1,3 +1,50 @@
+import { Dropdown, InputField } from '@/components';
+
+const options = [
+  '최신순',
+  '인기순',
+  '낮은가격순',
+  '높은가격순',
+  '낮은가격순',
+  '높은가격순',
+  '낮은가격순',
+  '높은가격순',
+];
+
 export default function Page() {
-  return <div>테스트페이지입니다</div>;
+  return (
+    <div>
+      <Dropdown options={options} sizeVariant='xs' />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Dropdown options={options} />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <InputField label='생년월일' />
+    </div>
+  );
 }
