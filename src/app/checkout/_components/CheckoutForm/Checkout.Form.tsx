69ebb47e282b215785520c14b55d7104422c77bd
김보경 <rlaqhrud6263@naver.com>
2024-06-23 04:25:27 +0900

    Feat: CheckoutForm UI 생성완료

--- src/app/checkout/_components/CheckoutForm/Checkout.Form.tsx
+++ src/app/checkout/_components/CheckoutForm/Checkout.Form.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import classNames from 'classnames/bind';
 
-import { Button, Dropdown, InputField } from '@/components';
+import { Button, Dropdown } from '@/components';
 
+import { Input, Label } from '@/components/parts';
 import styles from './CheckoutForm.module.scss';
 
 const cn = classNames.bind(styles);
@@ -12,80 +15,98 @@ export default function CheckoutForm() {
   return (
     <form className={cn('checkout-form')}>
       <article className={cn('form')}>
-        <div>
+        <div className={cn('item-box')}>
           <h1>주문 상품</h1>
-          <div>상품 컴포넌트</div>
+          <div className={cn('item')}>상품 컴포넌트</div>
         </div>
 
-        <div>
-          <span>총 주문금액</span>
-          <span>99,999원</span>
+        <div className={cn('price-box')}>
+          <p>총 주문금액</p>
+          <p className={cn('price')}>99,999원</p>
         </div>
 
-        <div>
+        <div className={cn('address-section')}>
           <h1>배송 주소</h1>
-          <div>
-            <div>
-              <div>
-                <h3>받는분</h3>
+          <div className={cn('address-box')}>
+            <div className={cn('address-wrap')}>
+              <div className={cn('address-key')}>
+                <p>받는분</p>
+                <p>연락처</p>
+                <p>배송 주소</p>
+              </div>
+              <div className={cn('address-value')}>
                 <p>오수야</p>
-              </div>
-              <div>
-                <h3>연락처</h3>
                 <p>010-1234-5678</p>
-              </div>
-              <div>
-                <h3>배송 주소</h3>
                 <p>서울시 인천시 일산구</p>
               </div>
+              {/* <div className={cn('address')} /> */}
             </div>
-            <Button type='button'>변경</Button>
+            <Button type='button' paddingVertical={8} width={72} radius={4}>
+              변경
+            </Button>
           </div>
-          <Dropdown options={DELIVERY_OPTIONS} />
+          <Dropdown options={DELIVERY_OPTIONS} sizeVariant='sm' placeholder='배송 요청 사항을 선택해 주세요.' />
         </div>
 
-        <div>
+        <div className={cn('discount-box')}>
           <h1>할인 혜택</h1>
-          <div>
-            <div>
-              <InputField label='쿠폰' />
-              <Button type='button'>최대 사용</Button>
-            </div>
-            <div>
-              <InputField label='포인트' />
-              <Button type='button'>최대 사용</Button>
-            </div>
+          <div className={cn('discount-field-box')}>
+            <Label className={cn('discount-field')} sizeVariant='sm'>
+              쿠폰
+              <div className={cn('discount-input')}>
+                <Dropdown
+                  className={cn('discount-coupon')}
+                  sizeVariant='sm'
+                  options={['사용 가능한 쿠폰이 없습니다.']}
+                  disabled
+                />
+                <Button className={cn('discount-button')} type='button'>
+                  최대 사용
+                </Button>
+              </div>
+            </Label>
+            <Label className={cn('discount-field')} sizeVariant='sm'>
+              포인트
+              <div className={cn('discount-input')}>
+                <Input sizeVariant='sm' readOnly value={0} />
+                <Button className={cn('discount-button')} type='button'>
+                  최대 사용
+                </Button>
+              </div>
+            </Label>
           </div>
-          <div>
-            <span>보유 포인트</span>
-            <p>0P</p>
-          </div>
+          <p className={cn('discount-point')}>
+            <span className={cn('point-title')}>보유 포인트</span>
+            0P
+          </p>
         </div>
 
-        <div>
+        <div className={cn('method-box')}>
           <h1>결제 수단</h1>
-          <div>
-            <h2>일반 결제</h2>
+          <h2 className={cn('method-default')}>일반 결제</h2>
+          <div className={cn('method-wrap')}>
             <Button type='button'>결제수단들</Button>
           </div>
-          <div>
-            <span>보유 포인트</span>
-            <p>0P</p>
-          </div>
+          <p className={cn('discount-point')}>
+            <span className={cn('point-title')}>보유 포인트</span>
+            0P
+          </p>
         </div>
 
-        <div>
+        <div className={cn('checkout-detail')}>
           <h1>결제 상세</h1>
-          <div>
-            <span>신용카드</span>
-            <p>99,999원</p>
-          </div>
+          <p className={cn('checkout-method')}>
+            <span className={cn('method-title')}>신용카드</span>
+            99,999원
+          </p>
         </div>
       </article>
 
-      <div>
+      <div className={cn('submit-box')}>
         <p>주문 내역을 확인하였으며, 정보 제공등에 동의합니다.</p>
-        <Button type='submit'>결제하기</Button>
+        <Button className={cn('submit-button')} type='submit'>
+          결제하기
+        </Button>
       </div>
     </form>
   );
