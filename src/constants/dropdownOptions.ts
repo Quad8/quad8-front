e8f4682df45cc7d63e747f76f4000100c91144b3
Young2un <popduddms@gmail.com>
2024-06-23 19:16:06 +0900

    Fix: 옵션추가

--- src/constants/dropdownOptions.ts
+++ src/constants/dropdownOptions.ts
@@ -1,5 +1,6 @@
 export const LIST_SORT_OPTIONS = [
   { label: '최신순', value: 'createdAt_desc' },
+  { label: '인기순', value: 'popular' },
   { label: '조회순', value: 'views_desc' },
   { label: '가격 낮은순', value: 'price_asc' },
   { label: '가격 높은순', value: 'price_desc' },
