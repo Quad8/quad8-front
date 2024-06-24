b02083ced95a808a7f0fe4d9aff323b4cf3e2b6a
김보경 <rlaqhrud6263@naver.com>
2024-06-24 18:45:38 +0900

    Chore: svg index.ts 수정

--- public/index.ts
+++ public/index.ts
@@ -9,9 +9,11 @@ export { default as CheckboxCircleIcon } from './svgs/checkboxCircle.svg';
 export { default as ChevronIcon } from './svgs/chevron.svg';
 export { default as CommentIcon } from './svgs/comment.svg';
 export { default as CreditCard } from './svgs/creditCard.svg';
-export { default as CrossCircleIcon } from './svgs/crossCircle.svg';
 export { default as Cube } from './svgs/cube.svg';
 
+export { default as CrossIcon } from './svgs/cross.svg';
+export { default as CrossCircleIcon } from './svgs/crossCircle.svg';
+export { default as DashIcon } from './svgs/dash.svg';
 export { default as DeleteIcon } from './svgs/delete.svg';
 export { default as EyeOffIcon } from './svgs/eyeOff.svg';
 export { default as EyeOnIcon } from './svgs/eyeOn.svg';
