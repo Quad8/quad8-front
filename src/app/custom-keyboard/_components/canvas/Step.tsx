7888b017f82da9b4e60b7492594f50ae8d953a42
armd482 <armd482@naver.com>
2024-06-30 23:46:41 +0900

    Style: Step 컴포넌트 크기 %로 변경

--- src/app/custom-keyboard/_components/canvas/Step.tsx
+++ src/app/custom-keyboard/_components/canvas/Step.tsx
@@ -36,20 +36,10 @@ export default function Step() {
   const SECOND_LINE_COMPLETED = stepStatus.switch !== 'pending' && stepStatus.keyCap !== 'pending';
 
   const handleClickStep = (value: CustomKeyboardStepTypes) => {
-    /* 
-      모든 단계는 처음부터 순차적으로 이동 -> 현재 keyCap단계에 있는 경우 switchType은 무조건 선택되어 있어 자유롭게 이동 가능 상태.
-      => switchType단계에서 선택되어있지 않은 경우, keyCap으로의 이동만 막으면 됨
-    */
-    if (currentStep === value) {
-      /* 현재 step을 누른 경우 */
+    if (currentStep === value || (value === 'keyCap' && !switchType)) {
       return;
     }
 
-    if (value === 'keyCap' && !switchType) {
-      /* keycap으로 이동할 때, switchType이 비어있는 경우 */
-      return;
-    }
-    /* Step 이동 시에, 현재 단계를 pending(swtichType의 경우 체크 안하고 이동할 때) 또는 completed처리, 이동하려는 Step은 current 처리 */
     if (currentStep === 'board') {
       captureCanvas(() => {
         updateStepStatus({
@@ -69,7 +59,7 @@ export default function Step() {
     <div className={cn('wrapper')}>
       {STEP_ICON.map((icon, i) => (
         <div key={icon.ID} className={cn('content-wrapper')}>
-          <button type='button' onClick={() => handleClickStep(icon.ID)}>
+          <button type='button' onClick={() => handleClickStep(icon.ID)} className={cn('step-button')}>
             <StepIcon status={icon.STATUS} number={i + 1} />
           </button>
           <div className={cn('content-text')}>{icon.NAME}</div>
